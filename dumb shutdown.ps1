c8d4dce79eb06112b89d7d8c050d465041458da2
phil-mel <120044497+phil-mel@users.noreply.github.com>
2022-12-20 14:03:27 +0000

    Add files via upload

---
+++ dumb shutdown.ps1
@@ -0,0 +1,12 @@
+﻿$realseconds = (get-date).second
+$minutes = (get-date).minute
+$hours = (get-date).hour
+$realminutes = (new-timespan -minutes $minutes).totalseconds
+$realhours = (new-timespan -hours $hours).totalseconds
+
+$trueseconds = ($realseconds + $realminutes + $realhours)
+
+$shutdowntime = (61200-$trueseconds)
+$shutdowntime
+
+shutdown -s -t $shutdowntime
